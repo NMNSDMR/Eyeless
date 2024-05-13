dd11f3ed556173f620db7efabe3f5dbd4af53a85
cerf <148485154+NMNSDMR@users.noreply.github.com>
2024-05-13 12:43:55 +0300

    Update reshinfo.py

--- cerf/reshinfo.py
+++ cerf/reshinfo.py
@@ -1,14 +1,28 @@
 from itertools import product
-i = 0
-def generate_combinations (letters, length) :
-    for combo in product (letters, repeat=length) :
-        yield ''.join (combo)
-letters = input ("Введите буквы без пробелов: ") 
-length = int (input ("Введите длину комбинации: "))|
-wewe = input("Введите слово которое нужно найти")
-for combo in generate_combinations (letters, length) :
-    i += 1
-    print (combo)
-    if combo == wewe:
-        print (i)
-        break
+
+def generate_combinations(letters, length):
+    """
+    Generate all combinations of the given letters up to the specified length.
+    
+    Args:
+    - letters: A string containing the input letters.
+    - length: The maximum length of combinations to generate.
+    
+    Returns:
+    - A generator yielding all combinations.
+    """
+    for combination_length in range(1, length + 1):
+        for combination in product(letters, repeat=combination_length):
+            yield ''.join(combination)
+
+def main():
+    input_letters = input("Enter the letters: ")
+    max_length = int(input("Enter the maximum combination length: "))
+    
+    combinations = generate_combinations(input_letters, max_length)
+    
+    for combination in combinations:
+        print(combination)
+
+if __name__ == "__main__":
+    main()
